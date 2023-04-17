a7276a595960203e01a607de11b961ebe8cbd050
pjw5521 <pjw5521@naver.com>
2023-04-17 20:40:05 +0900

    Feat : add getMashup맛집() & getMashup맛집byType()

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
+import mock from './mock-data.json';
+
 /**
  * 매쉬업 맛집 목록을 가져옵니다.
  */
 export function getMashup맛집() {
-  return [];
+  return mock.store_list;
+}
+
+export function getMashup맛집byType(t: string){
+  let data = new Array;
+  
+  for(let i =0 ; i < mock.store_list.length; i++){
+    if (mock.store_list[i].type == t){
+      data.push(mock.store_list[i]);
+    }
+  }
+  return data;
+}
+
+export function viewMap(){
+  
 }

94dde27a12cd9194ba31128d83fd05fed6d44993
hye-on <ain0103@naver.com>
2023-04-20 20:43:19 +0900

    feat : 맛집 입력 , store.json에 저장

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,8 @@
 // import { getMashup맛집 } from '.';
 import readline from 'readline';
-import { getMashup맛집, getMashup맛집byType } from '.';
+import { getMashup맛집, getMashup맛집byType, isExists } from '.';
+import fs from 'fs';
+import path from 'path';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -24,10 +26,41 @@ const mashelinChoice = (answer: string) => {
     rl.close();
   } else if (answer === '2') {
     rl.question('\n양식, 중식, 한식, 일식 중에 선택하세요', temp);
+  } else if (answer === '3') {
+    let nameT: any;
+    let addressT: any;
+    let typeT: any;
+
+    rl.question('이름을 입력하세요 !', input => {
+      nameT = input;
+      rl.question('주소 입력하세요 !', input => {
+        addressT = input;
+        rl.question('양식, 중식, 한식, 일식중에 선택해서 입력하세요(택1) ', input => {
+          typeT = input;
+          const storeT = {
+            name: nameT,
+            address: addressT,
+            type: typeT,
+          };
+          if (isExists(nameT, addressT)) {
+            console.log('이미 등록되어 있습니다.');
+            rl.close();
+          } else {
+            // console.log(storeT);
+            const filePath = path.join(__dirname, 'store.json');
+            const fileData = fs.readFileSync(filePath);
+            const fileJSON = fileData.toString();
+            const a = JSON.parse(fileJSON);
+            a.push(storeT);
+            fs.writeFileSync(filePath, JSON.stringify(a));
+            rl.close();
+          }
+        });
+      });
+    });
   } else {
     console.clear();
-    // console.log('y 또는 n만 입력하세요.');
-    rl.question('\n1 or 2 만 입력해요 (ꐦ •᷄ࡇ•᷅) ', mashelinChoice);
+    rl.question('\n1 or 2 or 3 만 입력해요 (ꐦ •᷄ࡇ•᷅) ', mashelinChoice);
   }
 };
 
@@ -36,9 +69,10 @@ const contents = (): void => {
   console.log('<<< (*●⁰ꈊ⁰●)ﾉ  mashelin-guide   (*●⁰ꈊ⁰●)ﾉ  >>>');
   console.log('\n============================================\n');
   console.log('1. 매시업 맛집 보기');
-  console.log('2. 매시업 맛집 검색하기\n');
+  console.log('2. 매시업 맛집 검색하기');
+  console.log('3. 매시업 맛집 등록하기\n');
 
-  rl.question('Enter only 1 or 2 : ', mashelinChoice);
+  rl.question('Enter only 1 or 2 or 3 : ', mashelinChoice);
 };
 
 const answerCallback = (answer: string) => {
